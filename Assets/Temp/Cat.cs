9bddfb2d14d0d9e16aa0cb3c0752c71ecf15ce5e
tjfkdu <0111407@kdu-online.com>
2016-09-27 23:14:13 +0800

    Zombie Plant Trap
    Zombie Plant Trap with rearm timer

---
+++ Assets/Temp/Cat.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cat : MonoBehaviour {
+	
+	public float speed;
+	public int hp = 3;
+	Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		velocity.x = Input.GetAxis ("Horizontal");
+		this.transform.Translate (Vector3.right * Time.deltaTime * speed);
+		Death ();
+	}
+
+	void Death(){
+		if (hp <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Temp/ZombiePlant.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombiePlant : MonoBehaviour {
+	bool deactive = false;
+	public float rearmTime;
+	private float rearmTimer;
+	public GameObject prefab;
+	Vector3 position = new Vector3(3.91f,0.658f,0);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (deactive) {
+			rearmTimer -= Time.deltaTime;
+			if (rearmTimer < 0) {
+				deactive = false;
+				this.GetComponent<SpriteRenderer>().color = Color.blue;
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.Alpha1)){ 
+			Instantiate (prefab,position, Quaternion.identity);
+		}
+	}
+
+	void OnTriggerEnter(Collider Other){
+		if(Other.CompareTag("Cat") && deactive == false){
+			Other.GetComponent<Cat> ().hp -= Other.GetComponent<Cat> ().hp;
+			deactive = true;
+			this.GetComponent<SpriteRenderer>().color = Color.red;
+			rearmTimer = rearmTime;
+		}
+	}
+}
