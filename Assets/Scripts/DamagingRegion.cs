00d7e31da884d1fc197dc162086472225c1182a1
Sharif Abdullah <0116302@kdu-online.com>
2016-10-12 06:51:17 +0800

    Added Freeze Gun and Laser
    Also modified Flamethrower to have flames instead of bullets

---
+++ Assets/Scripts/DamagingRegion.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DamagingRegion : MonoBehaviour {
+
+	public float damageOnEntry = 0.0f;
+	public float damagePerTick = 10f;
+	public float ticksPerSecond = 1.0f;
+	public float damageOnExit = 0.0f;
+
+	private List<Destructible> targets = new List<Destructible> ();
+
+	void Start () {
+		StartCoroutine (DamageTick ());
+	}
+
+	void OnTriggerEnter (Collider other) {
+		Destructible target = other.GetComponent<Destructible> ();
+		if (target != null) {
+			target.Damage (damageOnEntry);
+			targets.Add (target);
+		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		Destructible target = other.GetComponent<Destructible> ();
+		if (target != null) {
+			target.Damage (damageOnExit);
+			targets.Remove (target);
+		}
+	}
+
+	IEnumerator DamageTick () {
+		while (true) {
+			if (enabled) {
+				foreach (Destructible target in targets) {
+					target.Damage (damagePerTick);
+				}
+			}
+
+			yield return new WaitForSeconds (1.0f / ticksPerSecond);
+		}
+	}
+}
--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -1,24 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Flamethrower : Defense, ITargeter {
+[RequireComponent (typeof (LineRenderer))]
+public class Laser : Defense, ITargeter {
 
-	public Transform flame;
+	public Transform laserOrigin;
+	public float laserDistance = 16.0f;
+	public float damage = 10.0f;
+
+	private LineRenderer lineRenderer;
 
 	private Transform _target;
 	public Transform target {
 		get { return _target; }
 	}
 
-	public float fireDuration = 5.0f;
+	public float laserDuration = 0.1f;
 	private float cooldown = 0.0f;
-	public float cooldownDuration = 10.0f;
+	public float cooldownDuration = 5.0f;
 
 	GUIManager guiManager;
 
-	// Use this for initialization
-	void Start () {
+	void Awake () {
 		guiManager = GameObject.FindObjectOfType<GUIManager> ();
+		lineRenderer = GetComponent<LineRenderer> ();
 	}
 
 	// Update is called once per frame
@@ -71,15 +76,23 @@ public class Flamethrower : Defense, ITargeter {
 	}
 
 	IEnumerator Fire () {
-		DamagingRegion damage = flame.GetComponent<DamagingRegion> ();
-		if (damage != null) damage.enabled = true;
+		RaycastHit hit;
+		int layerMask = 1 | (1 << 10) | (1 << 11);
 
-		ParticleSystem effect = flame.GetComponent<ParticleSystem> ();
-		if (effect != null) effect.Play ();
+		if (Physics.Raycast (laserOrigin.position, -laserOrigin.up, out hit, laserDistance, layerMask, QueryTriggerInteraction.Ignore)) {
+			lineRenderer.SetPosition (0, laserOrigin.position);
+			lineRenderer.SetPosition (1, hit.point + (-laserOrigin.up * 0.25f));
+			lineRenderer.enabled = true;
 
-		yield return new WaitForSeconds (fireDuration);
+			if (hit.collider.CompareTag ("Enemy")) {
+				hit.collider.GetComponent<Enemy> ().Damage(damage);
+			}
 
-		if (damage != null) damage.enabled = false;
-		if (effect != null) effect.Stop ();
+			yield return new WaitForSeconds (laserDuration);
+
+			lineRenderer.enabled = false;
+		}
+
+		yield break;
 	}
 }
