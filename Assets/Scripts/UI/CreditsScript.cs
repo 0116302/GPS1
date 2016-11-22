ab13b62a6c557822052d88c33b47663e6ba47f5a
hcl1995 <how_choon_loong@hotmail.com>
2016-11-22 20:33:50 +0800

    Credits added

---
+++ Assets/Scripts/UI/CreditsScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class CreditsScript : MonoBehaviour
+{
+	public void Credits()
+	{
+		//Application.LoadLevel("Credits");
+		SceneManager.LoadScene("Credits");
+	}
+}
